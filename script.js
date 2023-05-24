af0310cd085bec8fb008ba4719168278d5cc8715
Simona Cartolina <simonacartolina@hotmail.com>
2023-05-24 11:35:33 +0200

    js snack-random 24/05

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-/*Generare numero casuale tra 1 e 100 COMPRESI.*/
-
-let casualNumber= Math.floor(Math.random()*100) +1;
-
-console.log(casualNumber);
-
-let i=prompt('Guess number from 0 to 100');
-
-
-/*Continuare a chiedere all'utente di indovinare il numero fino a quando non lo indovina.*/
-
-while(i!= casualNumber){
-    i=prompt('Try again!');
-    console.log(i);
-
-    if(i<casualNumber){
-
-        console.log('Your number is lower');
-        console.log(i);
-        i++;
-    }
-    else if(i>casualNumber){
-
-        console.log('Your number is higher');
-        console.log(i);
-        i++;
-    }
+/*Creare una funzione che restituisca un array di quattro numeri randomici diversi tra loro tra 1 e 5 inclusi*/
+function randomNumber(){
+    array=[1, 2, 3, 4, 5];
+    return array[i];
 }
-
-if(i=casualNumber){
-    console.log(i);
-    console.log('Congrats!');
-    alert("Congratulation! You won the game");
+for(i=0; i<3; i++){
+    let number= randomNumber();
+    console.log(number);
 }
-
-
-
-
-
-/*Una volta che ha indovinato, stampiamo in console il numero di tentativi totali.*/
