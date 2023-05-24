82d3e5cfa454bb5e236f57fd8fcefc16b60b7193
Simona Cartolina <simonacartolina@hotmail.com>
2023-05-24 11:40:23 +0200

    updated js snack-random

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /*Creare una funzione che restituisca un array di quattro numeri randomici diversi tra loro tra 1 e 5 inclusi*/
 function randomNumber(){
     array=[1, 2, 3, 4, 5];
-    return array[i];
+    let randomElement = array[Math.floor(Math.random() * array.length)];
+    return randomElement;
 }
 for(i=0; i<3; i++){
-    let number= randomNumber();
-    console.log(number);
+    let consoleNumber= randomNumber(i);
+    console.log(consoleNumber);
 }
