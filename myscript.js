703cbec73ab90e04d3006b0311c1868f7824cf9f
Simona Cartolina <126583566+SimonaCartolina@users.noreply.github.com>
2023-05-29 11:14:00 +0200

    snack oggetti 29/05

---
+++ myscript.js
@@ -0,0 +1,12 @@
+const userPlayer={
+    nome: 'Micheal',
+    cognome:'Jordan',
+    età: '30',
+    media:'20',
+    successpercentage:'5',
+    stoppate:'5',
+    tiri:'6',
+}
+for(let key in userPlayer){
+    console.log(key);
+}
