b055ec92ac345daaf23cbf10676112cd742fa933
Simona Cartolina <simonacartolina@hotmail.com>
2023-05-31 13:06:42 +0200

    paperino

--- myscript.js
+++ myscript.js
@@ -1,112 +1,11 @@
-/**
+const stringhe=['PaPErino', 'MinniE', 'pLUTo', 'Tigro', 'WINnie', 'PIPPo'];
 
-Crea un array composto da 15 automobili.
+let correctedStringhe=[];
 
-Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+stringhe.forEach((word)=>{
+const newWord= word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
+correctedStringhe.push(newWord);
+});
 
-Dividi le automobili in 3 array separati: 
 
-nel primo array solo le auto a benzina,  // ciclo for 
-nel secondo solo le auto a diesel,  // ciclo foreach
-nel terzo il resto delle auto.  //array.filter
-
-Infine stampa separatamente i 3 array.
-
- */
-
-let carList=[
-    car={
-    marca:'',
-    modello:'',
-    alimentazione:'benzina',
-    },
-
-    car={
-        marca:'',
-        modello:'',
-        alimentazione:'diesel',
-        },
-
-        car={
-            marca:'',
-            modello:'',
-            alimentazione:'benzina',
-            },
-
-            car={
-                marca:'',
-                modello:'',
-                alimentazione:'diesel',
-                },
-
-                car={
-                    marca:'',
-                    modello:'',
-                    alimentazione:'metano',
-                    },
-
-                    car={
-                        marca:'',
-                        modello:'',
-                        alimentazione:'gas',
-                        },
-
-                        car={
-                            marca:'',
-                            modello:'',
-                            alimentazione:'diesel',
-                            },
-
-                            car={
-                                marca:'',
-                                modello:'',
-                                alimentazione:'gas',
-                                },
-
-                                car={
-                                    marca:'',
-                                    modello:'',
-                                    alimentazione:'elettrica',
-                                    },
-
-                                    car={
-                                        marca:'',
-                                        modello:'',
-                                        alimentazione:'metano',
-                                        },
-
-                                        car={
-                                            marca:'',
-                                            modello:'',
-                                            alimentazione:'gas',
-                                            },
-
-                                            car={
-                                                marca:'',
-                                                modello:'',
-                                                alimentazione:'benzina',
-                                                },
-
-                                                car={
-                                                    marca:'',
-                                                    modello:'',
-                                                    alimentazione:'gpl',
-                                                    },
-
-                                                    car={
-                                                        marca:'',
-                                                        modello:'',
-                                                        alimentazione:'gas',
-                                                        },
-]
-
-for(let i=0; i<carList.length; i++){
-
-    if(carList[i].contains='benzina'){
-        const oilCars=[];
-        oilCars.push(carList[i]);
-        console.log(oilCars);
-
-    }
-    
-}
+    console.log(correctedStringhe);
